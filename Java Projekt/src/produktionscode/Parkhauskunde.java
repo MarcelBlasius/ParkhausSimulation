abcca61864e938b9f64cedf4c8ae7ef8e284941d
MarcelBlasius <blasius.marcel@gmail.com>
2020-05-05 19:45:33 +0200

    removed override´

--- Java Projekt/src/produktionscode/Parkhauskunde.java
+++ Java Projekt/src/produktionscode/Parkhauskunde.java
@@ -4,27 +4,24 @@ import java.time.Instant;
 
 public abstract class Parkhauskunde implements ParkhauskundeIF{
 	
-	
 	private Instant einfahrtszeit;
 	
 	public Parkhauskunde() {
 	}
 	
-	@Override
 	public Instant getEinfahrtszeit() {
 		return einfahrtszeit;
 	}
 
-	@Override
+	
 	public void einfahren() {
 		this.einfahrtszeit = Instant.now();
 	}
-	@Override
+
 	public void ausfahren() {
 		einfahrtszeit = null;
 	}
 
-	@Override
 	public String getParkplatz() {
 		return null;
 	}
